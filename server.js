98037ff7c9042d4f57ff7aa3a248665ca3f53f33
pzeise <phillip.zeise@gmail.com>
2022-04-16 14:25:30 -0400

    hiding my password/etc

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+
+
 
 
 const port = process.env.PORT || 4000
